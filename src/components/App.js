0c49e103f6353827fc34bdb0ab054b7abb8eec30
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-05 16:57:24 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,85 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-    render() {
+function App() {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [relationship, setRelationship] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  const handleNameChange = (e) => {
+    const { name, value } = e.target;
+    name === "name1" ? setName1(value) : setName2(value);
+  };
+
+  const calculateRelationship = () => {
+    // Remove common letters
+    const name1Set = new Set(name1);
+    const name2Set = new Set(name2);
+    const uniqueLetters1 = [...name1].filter((char) => !name2Set.has(char));
+    const uniqueLetters2 = [...name2].filter((char) => !name1Set.has(char));
+
+    // Calculate sum of lengths and take modulus by 6
+    const sumOfLengths = uniqueLetters1.length + uniqueLetters2.length;
+    const relationshipIndex = sumOfLengths % 6;
+
+    // Determine relationship based on index
+    const relationships = [
+      "Siblings",
+      "Friends",
+      "Love",
+      "Affection",
+      "Marriage",
+      "Enemy",
+    ];
+    const relationshipText = relationships[relationshipIndex];
+
+    setRelationship(relationshipText);
+  };
+
+  const clearInputs = () => {
+    setName1("");
+    setName2("");
+    setRelationship("");
+  };
+
+  return (
+    <div className="App">
+      <NameInput name={name1} onChange={handleNameChange} dataTestId="input1" />
+      <NameInput name={name2} onChange={handleNameChange} dataTestId="input2" />
+      <RelationshipCalculator
+        onClick={calculateRelationship}
+        dataTestId="calculate_relationship"
+      />
+      <ClearButton onClick={clearInputs} dataTestId="clear" />
+      {relationship && <h3 data-testid="answer">{relationship}</h3>}
+    </div>
+  );
 }
 
+function NameInput({ name, onChange, dataTestId }) {
+  return (
+    <input
+      type="text"
+      value={name}
+      onChange={onChange}
+      data-testid={dataTestId}
+    />
+  );
+}
+
+function RelationshipCalculator({ onClick, dataTestId }) {
+  return (
+    <button onClick={onClick} data-testid={dataTestId}>
+      Calculate Relationship
+    </button>
+  );
+}
+
+function ClearButton({ onClick, dataTestId }) {
+  return (
+    <button onClick={onClick} data-testid={dataTestId}>
+      Clear
+    </button>
+  );
+}
 
 export default App;
---
+++ src/components/Flame.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+const Flames = () => {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [relationship, setRelationship] = useState("");
+
+  const calculateRelationship = () => {
+    const name1Chars = name1.split("");
+    const name2Chars = name2.split("");
+
+    // Remove common letters from both names
+    const filteredName1 = name1Chars.filter(
+      (char) => !name2Chars.includes(char)
+    );
+    const filteredName2 = name2Chars.filter(
+      (char) => !name1Chars.includes(char)
+    );
+
+    const totalLength = filteredName1.length + filteredName2.length;
+    const relationshipStatus = totalLength % 6;
+
+    // Determine relationship status
+    switch (relationshipStatus) {
+      case 1:
+        setRelationship("Friends");
+        break;
+      case 2:
+        setRelationship("Love");
+        break;
+      case 3:
+        setRelationship("Affection");
+        break;
+      case 4:
+        setRelationship("Marriage");
+        break;
+      case 5:
+        setRelationship("Enemy");
+        break;
+      case 0:
+        setRelationship("Siblings");
+        break;
+      default:
+        setRelationship("Please enter valid input");
+    }
+  };
+
+  const clearForm = () => {
+    setName1("");
+    setName2("");
+    setRelationship("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter name 1"
+        value={name1}
+        onChange={(e) => setName1(e.target.value)}
+        data-testid="input1"
+      />
+      <input
+        type="text"
+        placeholder="Enter name 2"
+        value={name2}
+        onChange={(e) => setName2(e.target.value)}
+        data-testid="input2"
+      />
+      <button
+        onClick={calculateRelationship}
+        data-testid="calculate_relationship"
+      >
+        Calculate Relationship
+      </button>
+      <button onClick={clearForm} data-testid="clear">
+        Clear
+      </button>
+      <h3 data-testid="answer">{relationship}</h3>
+    </div>
+  );
+};
+
+export default Flames;
